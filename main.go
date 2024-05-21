91ea8ce3bd3e1837613ed969280ec2f0f6223400
Revanzavz <revanzavz@student.telkomuniversity.ac.id>
2024-05-21 22:05:38 +0700

    add eksemplar

--- main.go
+++ main.go
@@ -146,33 +146,7 @@ func MenuAdmin() {
 
 
 
-func tambahkanEksemplar(buku *tabBuku, i int) {
-	fmt.Println("Masukkan judul buku yang ingin ditambahkan eksemplarnya: ")
-	var keyword string
-	fmt.Scan(&keyword)
-	for j := 0; j < i; j++ {
-		if buku[j].judul == keyword {
-			fmt.Println("Buku ditemukan!")
-			fmt.Println("Judul buku: ", buku[j].judul)
-			fmt.Println("Pengarang: ", buku[j].pengarang)
-			fmt.Println("Nomor ISBN: ", buku[j].nomorISBN)
-			fmt.Println("Jumlah eksemplar: ", buku[j].jumlahEksemplar)
-			fmt.Println("Tahun terbit: ", buku[j].tahunTerbit)
-			fmt.Println(" ")
 
-			var tambah int
-			fmt.Println("Masukkan jumlah eksemplar yang ingin ditambahkan: ")
-			fmt.Scan(&tambah)
-			buku[j].jumlahEksemplar += tambah
-			fmt.Println("Jumlah eksemplar berhasil ditambahkan")
-			fmt.Println("Jumlah eksemplar saat ini: ", buku[j].jumlahEksemplar)
-			fmt.Println(" ")
-		} else {
-			fmt.Println("Buku tidak ditemukan")
-			fmt.Println(" ")
-		}
-	}
-}
 
 
 
