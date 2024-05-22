aeb456e2333d0994c1ee774f030766aff3ae5511
Revanzavz <revanzavz@student.telkomuniversity.ac.id>
2024-05-22 09:23:50 +0700

    judul sama update

--- inputBuku.go
+++ inputBuku.go
@@ -1,45 +1,48 @@
 package main
 
 import (
-	"fmt"
+    "fmt"
 )
 
 func inputBuku(buku *tabBuku, i *int) {
-	var again string
-	//var found bool
-	fmt.Println("Masukkan data buku:")
-	for again != "n" && *i < NMAX {
-		fmt.Print("Jika buku lebih dari 1 kata gunakan _ sebagai pengganti spasi\n")
-		fmt.Print("Masukkan judul buku: ")
-		fmt.Scan(&buku[*i].judul)
-		fmt.Print("Jika nama pengarang lebih dari 1 kata gunakan _ sebagai pengganti spasi\n")
-		fmt.Print("Masukkan nama pengarang: ")
-		fmt.Scan(&buku[*i].pengarang)
-		fmt.Print("Masukkan nomor ISBN: ")
-		fmt.Scan(&buku[*i].nomorISBN)
-		fmt.Print("Masukkan jumlah eksemplar: ")
-		fmt.Scan(&buku[*i].jumlahEksemplar)
-		fmt.Print("Masukkan tahun terbit: ")
-		fmt.Scan(&buku[*i].tahunTerbit)
+    var again string
+    fmt.Println("Masukkan data buku:")
+    for again != "n" && *i < NMAX {
+        fmt.Print("Jika buku lebih dari 1 kata gunakan _ sebagai pengganti spasi\n")
+        fmt.Print("Masukkan judul buku: ")
+        fmt.Scan(&buku[*i].judul)
+        fmt.Print("Jika nama pengarang lebih dari 1 kata gunakan _ sebagai pengganti spasi\n")
+        fmt.Print("Masukkan nama pengarang: ")
+        fmt.Scan(&buku[*i].pengarang)
+        fmt.Print("Masukkan nomor ISBN: ")
+        fmt.Scan(&buku[*i].nomorISBN)
+        fmt.Print("Masukkan jumlah eksemplar: ")
+        fmt.Scan(&buku[*i].jumlahEksemplar)
+        fmt.Print("Masukkan tahun terbit: ")
+        fmt.Scan(&buku[*i].tahunTerbit)
 
-		for j := 0; j < *i; j++ {
-			if buku[j].judul == buku[*i].judul {
-				buku[*i].jumlahEksemplar += buku[j].jumlahEksemplar
-				//found = true
-			}
-		}
+        found := false
+        for j := 0; j < *i; j++ {
+            if buku[j].judul == buku[*i].judul && buku[j].nomorISBN == buku[*i].nomorISBN && buku[j].pengarang == buku[*i].pengarang {
+                buku[j].jumlahEksemplar += buku[*i].jumlahEksemplar
+                found = true
+            }
+        }
+        
+        if !found {
+            // Jika buku baru, tambahkan ke array
+            *i++
+        }
 
-		
-		*i++
-		fmt.Println("Masukkan data buku lagi? (y/n) ")
-		fmt.Scan(&again)
+        fmt.Println("Masukkan data buku lagi? (y/n) ")
+        fmt.Scan(&again)
 
-		for again != "n" && again != "y" {
-			fmt.Println("Input tidak valid, silahkan input kembali")
-			fmt.Scan(&again)
-		}
+        for again != "n" && again != "y" {
+            fmt.Println("Input tidak valid, silahkan input kembali")
+            fmt.Scan(&again)
+        }
 
-		clearscreen()
-		fmt.Println(" ")
-	}
+        clearscreen()
+        fmt.Println(" ")
+    }
 }
